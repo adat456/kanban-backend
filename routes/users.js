df6a99985f15212480165092b5474b2ebd280e7e
Ada <adat456@gmail.com>
2023-04-17 09:39:52 -0400

    Add log out route

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,7 @@ router.post("/sign-up", async function(req, res, next) {
 
   try {
     const doc = await UserModel.create({ firstName, lastName, username, password, email });
+    // any errors caused by failure of validations set in the model will be forwarded to the catch block below
     if (doc) {
       // payload cannot just be doc._id... must be an object, so you need a key as well
       const token = jwt.sign({ id: doc._id }, process.env.JWT_SECRET, { expiresIn: "24h" });
@@ -47,6 +48,11 @@ router.post("/log-in", async function(req, res, next) {
   } catch(err) {
     res.status(404).send(err.message);
   };
+});
+
+/* log out user */
+router.get("/log-out", function(req, res, next) {
+  res.status(200).cookie("jwt", "", { maxAge: 1 }).send("Logged out.");
 });
 
 module.exports = router;
