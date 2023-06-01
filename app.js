f4f3e7b55298224c4409b2b7405d02fd19621393
Ada <adat456@gmail.com>
2023-05-31 23:32:45 -0400

    Adjust path in app.js for React Router

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/dist')));
 // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/dist', 'index.html'));
   });
 };
 
